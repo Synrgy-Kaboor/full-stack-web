85857022009b98b9f37aa95a13b9fade73a41fb1
TamaWira <asusrog20012001@gmail.com>
2024-01-03 18:04:43 +0800

    feat: update layanan tambahan feature

--- src/pages/Checkout/checkout.layananTambahan.tsx
+++ src/pages/Checkout/checkout.layananTambahan.tsx
@@ -1,7 +1,10 @@
+import Button from "../../components/ui/button";
+
 export default function LayananTambahan() {
     return (
         <div>
             <h1>Halaman Layanan Tambahan</h1>
+            <Button />
         </div>
     );
 }
