c9486d84d74b8abdfc5c593d4908e7ab3edd97e5
TamaWira <asusrog20012001@gmail.com>
2024-01-04 17:41:09 +0800

    feat: added AddOnsCard global component

--- src/pages/LayananTambahan/layananTambahan.tsx
+++ src/pages/LayananTambahan/layananTambahan.tsx
@@ -3,73 +3,9 @@ import {
   Grid,
   Typography,
   Container,
-  Card,
-  CardContent,
-  Divider,
-  Stack,
-  Link,
-  CardActions,
-  IconButton,
 } from '@mui/material';
-import HealthAndSafetyOutlinedIcon from '@mui/icons-material/HealthAndSafetyOutlined';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import ControlPointOutlinedIcon from '@mui/icons-material/ControlPointOutlined';
 
-function AddOnsCard({ title }: { title: string }) {
-  return (
-    <Card variant="outlined">
-      <CardContent sx={{ padding: 0 }}>
-        <Stack direction="row" m={2} alignItems="center">
-          <HealthAndSafetyOutlinedIcon fontSize="medium" />
-          <Typography sx={{ fontWeight: 'bold' }} ml={1}>
-            {title}
-          </Typography>
-        </Stack>
-        <Divider />
-        <Box p={2}>
-          <Stack direction="row">
-            <CheckCircleIcon fontSize="small" color="success" />
-            <Typography ml={1}>
-              Kompensasi hingga Rp 500 Juta untuk berbagai resiko tak terduga
-            </Typography>
-          </Stack>
-          <Stack direction="row" mb={2}>
-            <CheckCircleIcon fontSize="small" color="success" />
-            <Typography ml={1}>
-              Kompensasi hingga Rp 500 Juta untuk berbagai resiko tak terduga
-            </Typography>
-          </Stack>
-          <Link href="#" underline="none">
-            Baca Selengkapnya
-          </Link>
-        </Box>
-      </CardContent>
-      <CardActions disableSpacing sx={{ padding: '0' }}>
-        <Stack
-          direction="row"
-          px={2}
-          py={1}
-          m={0}
-          alignItems="center"
-          sx={{
-            backgroundColor: '#7B52AB',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Typography sx={{ fontWeight: 'bold', color: 'white' }}>
-            Rp 65.000/pax
-          </Typography>
-          <IconButton>
-            <ControlPointOutlinedIcon sx={{ color: 'white' }} />
-          </IconButton>
-        </Stack>
-      </CardActions>
-    </Card>
-  );
-}
+import AddOnsCard from '../../components/ui/AddOnsCard';
 
 export default function LayananTambahan() {
   return (
@@ -79,12 +15,14 @@ export default function LayananTambahan() {
       </Typography>
       <Grid container mt={0} spacing={2}>
         <Grid item xs={6}>
-          <AddOnsCard title={'Asuransi Perjalanan'} />
-          <AddOnsCard title={'Asuransi Bagasi'} />
-          <AddOnsCard title={'Proteksi Keterlambatan'} />
+          <AddOnsCard title={'Asuransi Perjalanan'} price={65000} />
+          <Box py={2}>
+            <AddOnsCard title={'Asuransi Bagasi'} price={100000} />
+          </Box>
+          <AddOnsCard title={'Proteksi Keterlambatan'} price={65000} />
         </Grid>
         <Grid item xs={6}>
-          <AddOnsCard title={'Asuransi Perjalanan'} />
+          <AddOnsCard title={'Asuransi Perjalanan'} price={65000} />
         </Grid>
       </Grid>
     </Container>
