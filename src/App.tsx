85ffa67b2156b978c30dcb34a4472da24c83412f
TamaWira <asusrog20012001@gmail.com>
2024-01-11 21:54:02 +0800

    fix: fix semicolon error

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import RegisterRoute from './pages/Auth/register/registerRoutes';
-import DetailPenumpang from './pages/DetailPenumpang/DetailPenumpang';
-import { LayananTambahan } from './pages/LayananTambahan';
-import { PilihJadwalSearch } from './pages/PilihJadwalSearch';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RegisterRoute from "./pages/Auth/register/registerRoutes";
+import DetailPenumpang from "./pages/DetailPenumpang/DetailPenumpang";
+import { LayananTambahan } from "./pages/LayananTambahan";
+import { PilihJadwalSearch } from "./pages/PilihJadwalSearch";
 
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './config/theme';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./config/theme";
 
 const router = createBrowserRouter([
   {
-    path: '/layanan-tambahan',
+    path: "/layanan-tambahan",
     element: <LayananTambahan />,
   },
   {
-    path: '/detail-penumpang',
+    path: "/detail-penumpang",
     element: <DetailPenumpang />,
   },
   {
-    path: '/register/*',
+    path: "/register/*",
     element: <RegisterRoute />,
   },
   {
-    path: '/pilih-jadwal-search',
+    path: "/pilih-jadwal-search",
     element: <PilihJadwalSearch />,
   },
 ]);
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
