aedd8b607ba33318e352808d7184517d5e790bb1
ignferry <ignasius.ferry01@gmail.com>
2024-01-18 14:28:27 +0700

    refactor: resolve eslint warnings

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,5 +1,5 @@
-import { Outlet } from "react-router-dom";
-import { Navbar } from "../components/ui";
+import { Outlet } from 'react-router-dom';
+import { Navbar } from '../components/ui';
 
 export default function DefaultLayout() {
     return (
