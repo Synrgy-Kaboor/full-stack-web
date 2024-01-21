707085ee32c4d56699710789038d9343fbd735aa
TamaWira <asusrog20012001@gmail.com>
2024-01-21 17:20:28 +0800

    feat: add navbar navigation to search page

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -7,34 +7,42 @@ import {
   Link,
   Avatar,
   Button,
-} from '@mui/material';
+} from "@mui/material";
 
-import KaboorIcon from '../../assets/kaboor.svg';
-import PlaneIcon from '../../assets/plane icon.png';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import KaboorIcon from "../../assets/kaboor.svg";
+import PlaneIcon from "../../assets/plane icon.png";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function UserLoggedIn() {
+  const navigate = useNavigate();
+
   return (
     <Stack direction="row" spacing={5} alignItems="center">
       {/* Notification */}
       <Stack direction="row" spacing={1} alignItems="center">
         <NotificationsNoneOutlinedIcon
           fontSize="large"
-          sx={{ color: 'primary.main' }}
+          sx={{ color: "primary.main" }}
         />
         <Typography color="black">Notifikasi</Typography>
       </Stack>
 
       {/* Pesanan */}
-      <Stack direction="row" spacing={1} alignItems="center">
+      <Stack
+        direction="row"
+        spacing={1}
+        alignItems="center"
+        onClick={() => navigate("pilih-jadwal-search")}
+        sx={{ "&:hover": { cursor: "pointer" } }}
+      >
         <img src={PlaneIcon} alt="" />
         <Typography color="black">Pesanan</Typography>
       </Stack>
 
       {/* Avatar */}
-      <Avatar sx={{ bgcolor: 'primary.main' }}>A</Avatar>
+      <Avatar sx={{ bgcolor: "primary.main" }}>A</Avatar>
     </Stack>
   );
 }
@@ -46,16 +54,16 @@ function UserNotLoggedIn() {
     <Stack direction="row" spacing={2} alignItems="center">
       <Button
         variant="outlined"
-        onClick={() => navigate('/register')}
-        sx={{ paddingInline: '2rem' }}
+        onClick={() => navigate("/register")}
+        sx={{ paddingInline: "2rem" }}
       >
         Register
       </Button>
       <Button
         variant="contained"
-        onClick={() => navigate('/login')}
+        onClick={() => navigate("/login")}
         sx={{
-          paddingInline: '2rem',
+          paddingInline: "2rem",
           backgroundImage: `linear-gradient(90deg, #7B52AB, #3A42FF)`,
         }}
       >
@@ -66,16 +74,16 @@ function UserNotLoggedIn() {
 }
 
 export default function Navbar() {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   console.log(token);
 
   return (
     <AppBar
       position="static"
       sx={{
-        backgroundColor: 'white',
-        boxShadow: 'none',
-        marginBottom: '1rem',
+        backgroundColor: "white",
+        boxShadow: "none",
+        marginBottom: "1rem",
       }}
     >
       <Container>
@@ -83,7 +91,7 @@ export default function Navbar() {
           <Stack
             direction="row"
             justifyContent="space-between"
-            sx={{ width: '100%' }}
+            sx={{ width: "100%" }}
           >
             {/* Logo */}
             <Link href="/">
