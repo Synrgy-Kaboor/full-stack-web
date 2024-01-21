17bfa882ae13a20328b41b07d5c756e65fc7f44c
TamaWira <asusrog20012001@gmail.com>
2024-01-22 00:22:25 +0800

    feat: add dynamic pop up

--- src/pages/LayananTambahan/LayananTambahan.tsx
+++ src/pages/LayananTambahan/LayananTambahan.tsx
@@ -1,26 +1,36 @@
-import { Box, Grid, Typography, Container, Stack, Button } from '@mui/material';
-import { AddOnsCard } from '../../components/ui/';
-import theme from '../../config/theme';
-import FlightDetailCard from '../../components/shared/Pemesanan/FlightDetailCard';
+import { Box, Grid, Typography, Container, Stack, Button } from "@mui/material";
+import { AddOnsCard } from "../../components/ui/";
+import theme from "../../config/theme";
+import { IDetailAsuransi, detailAsuransi } from "./detailAsuransi";
+import FlightDetailCard from "../../components/shared/Pemesanan/FlightDetailCard";
 
 export default function LayananTambahan() {
+  const addOnsCardContent = detailAsuransi.map(
+    (insuranceDetail: IDetailAsuransi) => {
+      return (
+        <AddOnsCard
+          key={insuranceDetail.id}
+          id={insuranceDetail.id}
+          title={insuranceDetail.title}
+          price={insuranceDetail.price}
+          descriptions={insuranceDetail.descriptions}
+          details={insuranceDetail.details}
+        />
+      );
+    }
+  );
+
   return (
     <>
-      <Container sx={{ paddingBlockEnd: '2rem' }}>
-        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+      <Container sx={{ paddingBlockEnd: "2rem" }}>
+        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
           Layanan Tambahan
         </Typography>
         <Grid container mt={0} spacing={2}>
           {/* Left Cards */}
           <Grid item md={6} xs={12}>
-            <Stack spacing={2}>
-              <AddOnsCard title={'Asuransi Perjalanan'} price={65000} />
-              <AddOnsCard title={'Asuransi Bagasi'} price={100000} />
-              <AddOnsCard title={'Proteksi Keterlambatan'} price={65000} />
-            </Stack>
+            <Stack spacing={2}>{addOnsCardContent}</Stack>
           </Grid>
-
-          {/* Right Cards */}
           <Grid item md={6} xs={12}>
             <Stack spacing={2}>
               <FlightDetailCard />
@@ -36,20 +46,20 @@ export default function LayananTambahan() {
                     variant="h5"
                     sx={{
                       background: theme.palette.gradients?.horizontal,
-                      backgroundClip: 'text',
-                      color: 'transparent',
-                      fontWeight: 'bold',
+                      backgroundClip: "text",
+                      color: "transparent",
+                      fontWeight: "bold",
                     }}
                   >
                     Rp 2.350.000
                   </Typography>
                 </Box>
-                <Box sx={{ width: '40%' }}>
+                <Box sx={{ width: "40%" }}>
                   <Button
                     variant="contained"
                     sx={{
                       background: theme.palette.gradients?.horizontal,
-                      width: '100%',
+                      width: "100%",
                     }}
                   >
                     Bayar
