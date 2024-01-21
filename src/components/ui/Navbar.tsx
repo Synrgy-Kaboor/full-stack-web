2373bb85941c34d186619aeb560c72eb2321c39c
TamaWira <asusrog20012001@gmail.com>
2024-01-21 16:47:01 +0800

    feat: add auth feature to navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -6,41 +6,92 @@ import {
   Stack,
   Link,
   Avatar,
-} from '@mui/material';
+  Button,
+} from "@mui/material";
 
-import KaboorIcon from '../../assets/kaboor.svg';
-import PlaneIcon from '../../assets/plane icon.png';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import KaboorIcon from "../../assets/kaboor.svg";
+import PlaneIcon from "../../assets/plane icon.png";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+
+import { useNavigate } from "react-router-dom";
+
+function UserLoggedIn() {
+  return (
+    <Stack direction="row" spacing={5} alignItems="center">
+      {/* Notification */}
+      <Stack direction="row" spacing={1} alignItems="center">
+        <NotificationsNoneOutlinedIcon
+          fontSize="large"
+          sx={{ color: "primary.main" }}
+        />
+        <Typography color="black">Notifikasi</Typography>
+      </Stack>
+
+      {/* Pesanan */}
+      <Stack direction="row" spacing={1} alignItems="center">
+        <img src={PlaneIcon} alt="" />
+        <Typography color="black">Pesanan</Typography>
+      </Stack>
+
+      {/* Avatar */}
+      <Avatar sx={{ bgcolor: "primary.main" }}>A</Avatar>
+    </Stack>
+  );
+}
+
+function UserNotLoggedIn() {
+  const navigate = useNavigate();
+
+  return (
+    <Stack direction="row" spacing={2} alignItems="center">
+      <Button
+        variant="outlined"
+        onClick={() => navigate("/register")}
+        sx={{ paddingInline: "2rem" }}
+      >
+        Register
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => navigate("/login")}
+        sx={{
+          paddingInline: "2rem",
+          backgroundImage: `linear-gradient(90deg, #7B52AB, #3A42FF)`,
+        }}
+      >
+        Login
+      </Button>
+    </Stack>
+  );
+}
 
 export default function Navbar() {
+  const token = localStorage.getItem("token");
+  console.log(token);
+
   return (
     <AppBar
       position="static"
       sx={{
-        backgroundColor: 'white',
-        boxShadow: 'none',
-        marginBottom: '1rem',
+        backgroundColor: "white",
+        boxShadow: "none",
+        marginBottom: "1rem",
       }}
     >
       <Container>
         <Toolbar disableGutters>
-          <Link href="/">
-            <img src={KaboorIcon} alt="" />
-          </Link>
-          <Typography sx={{ flexGrow: 1 }}></Typography>
-          <Stack direction="row" spacing={4} alignItems="center">
-            <Stack direction="row" spacing={1} alignItems="center">
-              <NotificationsNoneOutlinedIcon
-                fontSize="large"
-                sx={{ color: 'primary.main' }}
-              />
-              <Typography color="black">Notifikasi</Typography>
-            </Stack>
-            <Stack direction="row" spacing={1} alignItems="center">
-              <img src={PlaneIcon} alt="" />
-              <Typography color="black">Pesanan</Typography>
-            </Stack>
-            <Avatar sx={{ bgcolor: 'primary.main' }}>A</Avatar>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            sx={{ width: "100%" }}
+          >
+            {/* Logo */}
+            <Link href="/">
+              <img src={KaboorIcon} alt="" />
+            </Link>
+
+            {/* Content */}
+            {token ? <UserLoggedIn /> : <UserNotLoggedIn />}
           </Stack>
         </Toolbar>
       </Container>
