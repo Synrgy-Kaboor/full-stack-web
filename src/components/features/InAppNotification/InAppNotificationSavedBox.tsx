dce8d3a237d5e511e90eaf37954cdd78954e4deb
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2024-01-26 17:36:22 +0700

    fix: lint problems when merge

--- src/components/features/InAppNotification/InAppNotificationSavedBox.tsx
+++ src/components/features/InAppNotification/InAppNotificationSavedBox.tsx
@@ -1,50 +1,48 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/quotes */
-import { Box, Stack, Typography, Divider, IconButton } from "@mui/material";
+import { Box, Stack, Typography, Divider, IconButton } from '@mui/material';
 import {
   CalendarMonthOutlined,
   DirectionsWalkOutlined,
   FlightClassOutlined,
   FlightTakeoffOutlined,
   MoreHorizOutlined,
-} from "@mui/icons-material";
-import theme from "../../../config/theme";
-import { useState } from "react";
+} from '@mui/icons-material';
+import theme from '../../../config/theme';
+import { useState } from 'react';
 const InAppNotificationSavedBox = () => {
   const [popUpDetailVisible, setPopUpDetailVisible] = useState(false);
   return (
     <>
-      <Box borderRadius={1} border={"1px solid #C2C2C2"} py={2}>
+      <Box borderRadius={1} border={'1px solid #C2C2C2'} py={2}>
         <Stack gap={1}>
           <Stack px={3}>
             <Stack
-              direction={"row"}
-              alignItems={"center"}
-              justifyContent={"space-between"}
+              direction={'row'}
+              alignItems={'center'}
+              justifyContent={'space-between'}
               gap={1.25}
               mb={1}
             >
-              <Stack direction={"row"} alignItems={"center"} gap={2.5}>
-                <Typography variant="h6" fontWeight={600} color={"#505050"}>
+              <Stack direction={'row'} alignItems={'center'} gap={2.5}>
+                <Typography variant="h6" fontWeight={600} color={'#505050'}>
                   Surabaya (SUB)
                 </Typography>
                 <FlightTakeoffOutlined />
-                <Typography variant="h6" fontWeight={600} color={"#505050"}>
+                <Typography variant="h6" fontWeight={600} color={'#505050'}>
                   Jakarta (CGK)
                 </Typography>
               </Stack>
-              <Box position={"relative"}>
+              <Box position={'relative'}>
                 <IconButton onClick={() => setPopUpDetailVisible(!popUpDetailVisible)}>
                   <MoreHorizOutlined />
                 </IconButton>
-                <Box borderRadius={1} border={"1px solid #C2C2C2"} sx={{background: "#FFF"}} py={2.25} position={"absolute"} right={0} width={"max-content"} display={popUpDetailVisible ? "flex" : "none"}>
+                <Box borderRadius={1} border={'1px solid #C2C2C2'} sx={{background: '#FFF'}} py={2.25} position={'absolute'} right={0} width={'max-content'} display={popUpDetailVisible ? 'flex' : 'none'}>
                   <Stack gap={1}>
                     <Typography
                       variant="h6"
                       sx={{
                         background: theme.palette.gradients?.diagonal,
-                        backgroundClip: "text",
-                        color: "transparent",
+                        backgroundClip: 'text',
+                        color: 'transparent',
                       }}
                       fontWeight={600}
                       px={3.5}
@@ -55,9 +53,9 @@ const InAppNotificationSavedBox = () => {
                     <Typography
                       variant="h6"
                       sx={{
-                        background: "#CB3A31",
-                        backgroundClip: "text",
-                        color: "transparent",
+                        background: '#CB3A31',
+                        backgroundClip: 'text',
+                        color: 'transparent',
                       }}
                       fontWeight={600}
                       px={3.5}
@@ -68,20 +66,20 @@ const InAppNotificationSavedBox = () => {
                 </Box>
               </Box>
             </Stack>
-            <Stack direction={"row"} gap={1.7}>
-              <Stack direction={"row"} gap={0.5} color={"#9E9E9E"}>
+            <Stack direction={'row'} gap={1.7}>
+              <Stack direction={'row'} gap={0.5} color={'#9E9E9E'}>
                 <CalendarMonthOutlined />
                 <Typography variant="subtitle1" fontWeight={600}>
                   4 Des 2024
                 </Typography>
               </Stack>
-              <Stack direction={"row"} gap={0.5} color={"#9E9E9E"}>
+              <Stack direction={'row'} gap={0.5} color={'#9E9E9E'}>
                 <DirectionsWalkOutlined />
                 <Typography variant="subtitle1" fontWeight={600}>
                   1 Orang
                 </Typography>
               </Stack>
-              <Stack direction={"row"} gap={0.5} color={"#9E9E9E"}>
+              <Stack direction={'row'} gap={0.5} color={'#9E9E9E'}>
                 <FlightClassOutlined />
                 <Typography variant="subtitle1" fontWeight={600}>
                   Ekonomi
@@ -95,8 +93,8 @@ const InAppNotificationSavedBox = () => {
             variant="subtitle1"
             sx={{
               background: theme.palette.gradients?.diagonal,
-              backgroundClip: "text",
-              color: "transparent",
+              backgroundClip: 'text',
+              color: 'transparent',
             }}
             fontWeight={700}
           >
