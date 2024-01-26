7a5c7526c0866f375aa04db7fcc1bea009246637
Ignasius Ferry <ignasius.ferry01@gmail.com>
2024-01-26 14:57:01 +0700

    feat: metode pembayaran

---
+++ src/components/features/MetodePembayaran/FormVoucher.tsx
@@ -0,0 +1,9 @@
+import { Stack, Typography } from '@mui/material';
+
+export default function FormVoucher() {
+  return (
+    <Stack p={2}>
+      <Typography>UNIMPLEMENTED</Typography>
+    </Stack>
+  )
+}
