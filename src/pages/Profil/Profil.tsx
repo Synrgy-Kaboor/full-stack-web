6b4d6ac119f529ee01b48f00e8da23eb34b6798d
SpexID <ngurahadipermana15@gmail.com>
2024-01-26 16:23:38 +0800

    feat: add change profile information layout component

--- src/pages/Profil/Profil.tsx
+++ src/pages/Profil/Profil.tsx
@@ -1,14 +1,16 @@
 import { Container } from '@mui/material';
 import Sidebar from '../../components/shared/Profil/Sidebar';
-import { Outlet } from 'react-router-dom';
+import  ChangeProfile  from '../../components/ui/ChangeProfile';
+import { Outlet, useLocation } from 'react-router-dom';
 
 
 export default function Profil () {
-
+    const location = useLocation();
     return (
         <>
         <Container maxWidth={'xl'} sx={{display: 'flex', gap: '16px'}}>
             <Sidebar/>
+            {location.pathname === '/profil/' && <ChangeProfile />}
             <Outlet/>
         </Container>
         </>
