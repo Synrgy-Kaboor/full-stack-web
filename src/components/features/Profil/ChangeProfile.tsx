d1c374ac735ed00fe3ebb9c300cbed8b3c377f66
SpexID <ngurahadipermana15@gmail.com>
2024-01-31 19:00:48 +0800

    update: add profile image

--- src/components/features/Profil/ChangeProfile.tsx
+++ src/components/features/Profil/ChangeProfile.tsx
@@ -1,7 +1,9 @@
-import PrimaryButton from '../core/primaryButton';
+import PrimaryButton from '../../core/primaryButton';
 import { Stack, Typography} from '@mui/material';
 import styled from 'styled-components';
-import icon from '../../assets/Chevron-Down.svg'; 
+import icon from '../../../assets/Chevron-Down.svg'; 
+import camera from '../../../assets/camera icon.png'; 
+import profile from '../../../assets/profile-img.png'; 
 
 
 const Select = styled.select`
@@ -79,10 +81,25 @@ const InputAlamat = styled.textarea`
     outline: 1px solid rgba(123, 82, 171, 0.5);
   }
 `;
-
+const RoundImage = styled.img`
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+   cursor: pointer;
+   border: 1px solid var(--Gray-03, #E7E3DA);
+`;
+const SmallImage = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 20px;
+  cursor: pointer;
+  margin-top: -50px;
+  margin-right: -10px;
+`;
 export default function ChangeProfile (){
     return ( 
         <>
+        
         <Stack direction={'column'} maxWidth={'880px'} alignItems={'start'} sx={{width: '100%', height:'100%', padding: '34px 43px', borderRadius:'8px',
             border:'1px solid #C2C2C2', background: 'white'}} gap={2} >
                     
@@ -90,6 +107,18 @@ export default function ChangeProfile (){
                     color: '#000', fontFamily: 'Open Sans', fontSize: '24px', fontWeight: 700, letterSpacing: '-0.5px'
                 }}>
             Informasi Pribadi</Typography>
+
+            <Stack direction={'column'} alignItems={'center'} sx={{width: '100%', marginBottom:'16px'}} gap={1}>
+            <Typography sx={{
+                    color: '#505050', fontFamily: 'Open Sans', fontSize: '20px', fontWeight: 600, letterSpacing: '-0.75px'
+                }}>
+                    Foto Profil
+                </Typography>
+            <Stack alignItems={'flex-end'}>
+            <RoundImage src={profile} alt="Round Image"/>
+            <SmallImage src={camera} alt="Round Image"/>
+            </Stack>
+            </Stack>
             <Stack direction={'column'} alignItems={'stretch'} sx={{width: '100%', marginBottom:'16px'}} gap={1}>
                 <Typography sx={{
                     color: '#505050', fontFamily: 'Open Sans', fontSize: '20px', fontWeight: 600, letterSpacing: '-0.75px'
