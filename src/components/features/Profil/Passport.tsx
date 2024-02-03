0d1d4cbaabfff35f3bb50e5400cdd19747a3aab1
TamaWira <asusrog20012001@gmail.com>
2024-02-03 12:44:28 +0800

    feat: Finished Slicing UI

--- src/components/features/Profil/Passport.tsx
+++ src/components/features/Profil/Passport.tsx
@@ -1,20 +1,34 @@
-import { Stack, Typography, OutlinedInput, Button } from "@mui/material";
-import theme from "../../../config/theme";
+import {
+  Stack,
+  Typography,
+  OutlinedInput,
+  Button,
+  //   InputAdornment,
+} from '@mui/material';
+
+import { DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
+import theme from '../../../config/theme';
+
+// import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 export default function Passport() {
   return (
     <Stack
       p={4}
-      bgcolor={"white"}
+      bgcolor={'white'}
       borderRadius={2}
-      sx={{ width: "100%", border: "1px solid #C2C2C2" }}
+      sx={{ width: '70%', border: '1px solid #C2C2C2' }}
     >
-      <Typography variant="h6" fontWeight={"bold"}>
+      <Typography variant="h6" fontWeight={'bold'}>
         Paspor
       </Typography>
 
       <Typography mt={2}>Detail Paspor</Typography>
-      <Typography color={"gray"} variant="subtitle2">
+      <Typography color={'gray'} variant="subtitle2">
         Isi semua kolom dengan data paspor yang berlaku ya
       </Typography>
 
@@ -40,22 +54,22 @@ export default function Passport() {
         <Typography mt={2} gutterBottom>
           Tanggal Kadaluwarsa
         </Typography>
-        <OutlinedInput
-          size="medium"
-          placeholder="Masukkan tanggal kadaluwarsa"
-          fullWidth
-        />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoItem>
+            <DesktopDatePicker sx={{ width: '100%' }} />
+          </DemoItem>
+        </LocalizationProvider>
 
         <Typography mt={2} gutterBottom>
           Negara yang Menerbitkan
         </Typography>
         <OutlinedInput size="medium" placeholder="Masukkan Negara" fullWidth />
 
-        <Stack alignItems={"end"} sx={{ width: "100%" }} my={2}>
+        <Stack alignItems={'end'} sx={{ width: '100%' }} my={2}>
           <Button
             variant="contained"
             sx={{
-              width: "20%",
+              width: '20%',
               background: theme.palette.gradients?.horizontal,
             }}
           >
