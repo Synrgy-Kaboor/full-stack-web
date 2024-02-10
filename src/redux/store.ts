7a9d73b871195d4e1a0d8e6185eaac19405cbeec
Ignasius Ferry <ignasius.ferry01@gmail.com>
2024-02-10 21:09:42 +0700

    fix: delete unused redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import metodePembayaranReducer from './slices/MetodePembayaran';
 import bookingReducer from './slices/Booking';
 
 export const store = configureStore({
     reducer: {
-        metodePembayaran: metodePembayaranReducer,
         booking: bookingReducer
     }
 });
