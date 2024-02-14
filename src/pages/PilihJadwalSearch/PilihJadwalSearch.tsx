ea1cbc392552c21b88e383f898ae5588eca03bc0
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2024-02-14 17:26:52 +0700

    style: fix padding problems

--- src/pages/PilihJadwalSearch/PilihJadwalSearch.tsx
+++ src/pages/PilihJadwalSearch/PilihJadwalSearch.tsx
@@ -1,10 +1,10 @@
 import {
   Grid,
   Typography,
-  useTheme,
-  useMediaQuery,
   Stack,
   Button,
+  Container,
+  Box,
 } from '@mui/material';
 import bgPesawat from '../../assets/ikhroma-bg-pesawat.jpg';
 import CardFilterPlaneSchedule from '../../components/ui/CardFilterPlaneSchedule';
@@ -15,8 +15,6 @@ import { NotificationAddOutlined } from '@mui/icons-material';
 
 const PilihJadwalSearch = () => {
   const navigate = useNavigate();
-  const newTheme = useTheme();
-  const sizeScreen = useMediaQuery(newTheme.breakpoints.up('md'));
   const originAirport = useAppSelector(
     (state) => state.searchJadwal.originAirportCode
   );
@@ -48,84 +46,82 @@ const PilihJadwalSearch = () => {
   };
 
   return (
-    <>
-      <Grid
-        container
-        sx={{
-          background: `
-    linear-gradient(270deg, rgba(58, 66, 255, 0.50) 0%, rgba(123, 82, 171, 0.50) 100%),
-    url(${bgPesawat})`,
-          paddingBlock: '4rem',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          height: '100vh',
-        }}
-        rowSpacing={2}
-        alignItems={'center'}
-      >
-        <Grid container item md={6}>
-          <Typography
-            variant="h3"
-            color={'white'}
-            fontWeight={'800'}
-            fontStyle={'normal'}
-            fontFamily={'Open Sans'}
-            pl={sizeScreen ? 17 : 2}
-          >
-            Tiket Pesawat Murah & Promo Hari Ini
-          </Typography>
-        </Grid>
-        <Grid
-          container
-          item
-          md={6}
-          justifyContent={'center'}
-          alignItems={'center'}
-          position={'relative'}
-          direction={'column'}
-        >
-          <CardFilterPlaneSchedule
-            sliderOn={false}
-            textSubmit="Cari"
-            onSubmit={handleSubmit}
-            homecomingOn={true}
-          />
-          <Stack direction={'row'} alignItems={'center'} spacing={4} mt={1.5}>
+    <Box
+      sx={{
+        background: `
+linear-gradient(270deg, rgba(58, 66, 255, 0.50) 0%, rgba(123, 82, 171, 0.50) 100%),
+url(${bgPesawat})`,
+        paddingBlock: '2rem',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '100vh',
+      }}
+    >
+      <Container>
+        <Grid container rowSpacing={2} alignItems={'center'}>
+          <Grid container item md={6}>
             <Typography
-              variant="h6"
-              fontWeight={600}
-              fontStyle={'normal'}
+              variant="h3"
               color={'white'}
+              fontWeight={'800'}
+              fontStyle={'normal'}
               fontFamily={'Open Sans'}
             >
-              Ingin harga yang sesuai?
+              Tiket Pesawat Murah & Promo Hari Ini
             </Typography>
-            <Button
-              variant="outlined"
-              sx={{
-                background: `white`,
-                color: `linear-gradient(270deg, rgba(58, 66, 255, 0.50) 0%, rgba(123, 82, 171, 0.50) 100%)`,
-                backgroundClip: 'border-box',
-                borderRadius: '24px',
-                padding: '8px 10px',
-                textTransform: 'none',
-                '&:hover': {
+          </Grid>
+          <Grid
+            container
+            item
+            md={6}
+            justifyContent={'center'}
+            alignItems={'flex-end'}
+            position={'relative'}
+            direction={'column'}
+          >
+            <CardFilterPlaneSchedule
+              sliderOn={false}
+              textSubmit="Cari"
+              onSubmit={handleSubmit}
+              homecomingOn={true}
+            />
+            <Stack direction={'row'} alignItems={'center'} spacing={4} mt={1.5}>
+              <Typography
+                variant="h6"
+                fontWeight={600}
+                fontStyle={'normal'}
+                color={'white'}
+                fontFamily={'Open Sans'}
+              >
+                Ingin harga yang sesuai?
+              </Typography>
+              <Button
+                variant="outlined"
+                sx={{
                   background: `white`,
-                  opacity: '0.8',
-                },
-                fontSize: '16px',
-                fontWeight: '600',
-                fontStyle: 'normal',
-              }}
-              onClick={() => navigate('/profil/saved-price-alert')}
-              startIcon={<NotificationAddOutlined />}
-            >
-              Notifikasi Harga
-            </Button>
-          </Stack>
+                  color: `linear-gradient(270deg, rgba(58, 66, 255, 0.50) 0%, rgba(123, 82, 171, 0.50) 100%)`,
+                  backgroundClip: 'border-box',
+                  borderRadius: '24px',
+                  padding: '8px 10px',
+                  textTransform: 'none',
+                  '&:hover': {
+                    background: `white`,
+                    opacity: '0.8',
+                  },
+                  fontSize: '16px',
+                  fontWeight: '600',
+                  fontStyle: 'normal',
+                }}
+                onClick={() => navigate('/profil/saved-price-alert')}
+                startIcon={<NotificationAddOutlined />}
+              >
+                Notifikasi Harga
+              </Button>
+            </Stack>
+          </Grid>
         </Grid>
-      </Grid>
-    </>
+      </Container>
+    </Box>
   );
 };
 
