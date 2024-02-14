fac7fdf7a67957216e286158c70e1d50610218f3
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2024-02-14 16:58:30 +0700

    fix: add navigate to price alert

--- src/pages/PilihJadwalSearch/PilihJadwalSearch.tsx
+++ src/pages/PilihJadwalSearch/PilihJadwalSearch.tsx
@@ -1,9 +1,17 @@
-import { Grid, Typography, useTheme, useMediaQuery } from '@mui/material';
+import {
+  Grid,
+  Typography,
+  useTheme,
+  useMediaQuery,
+  Stack,
+  Button,
+} from '@mui/material';
 import bgPesawat from '../../assets/ikhroma-bg-pesawat.jpg';
 import CardFilterPlaneSchedule from '../../components/ui/CardFilterPlaneSchedule';
 import { type CardFilterPlaneScheduleType as filterType } from '../../types/CardFilterPlaneScheduleProps';
 import { useAppSelector } from '../../redux/hooks';
 import { useNavigate } from 'react-router-dom';
+import { NotificationAddOutlined } from '@mui/icons-material';
 
 const PilihJadwalSearch = () => {
   const navigate = useNavigate();
@@ -57,12 +65,12 @@ const PilihJadwalSearch = () => {
       >
         <Grid container item md={6}>
           <Typography
-            variant='h3'
+            variant="h3"
             color={'white'}
             fontWeight={'800'}
             fontStyle={'normal'}
             fontFamily={'Open Sans'}
-            pl={sizeScreen ? 6 : 2}
+            pl={sizeScreen ? 17 : 2}
           >
             Tiket Pesawat Murah & Promo Hari Ini
           </Typography>
@@ -74,13 +82,47 @@ const PilihJadwalSearch = () => {
           justifyContent={'center'}
           alignItems={'center'}
           position={'relative'}
+          direction={'column'}
         >
           <CardFilterPlaneSchedule
             sliderOn={false}
-            textSubmit='Cari'
+            textSubmit="Cari"
             onSubmit={handleSubmit}
             homecomingOn={true}
           />
+          <Stack direction={'row'} alignItems={'center'} spacing={4} mt={1.5}>
+            <Typography
+              variant="h6"
+              fontWeight={600}
+              fontStyle={'normal'}
+              color={'white'}
+              fontFamily={'Open Sans'}
+            >
+              Ingin harga yang sesuai?
+            </Typography>
+            <Button
+              variant="outlined"
+              sx={{
+                background: `white`,
+                color: `linear-gradient(270deg, rgba(58, 66, 255, 0.50) 0%, rgba(123, 82, 171, 0.50) 100%)`,
+                backgroundClip: 'border-box',
+                borderRadius: '24px',
+                padding: '8px 10px',
+                textTransform: 'none',
+                '&:hover': {
+                  background: `white`,
+                  opacity: '0.8',
+                },
+                fontSize: '16px',
+                fontWeight: '600',
+                fontStyle: 'normal',
+              }}
+              onClick={() => navigate('/profil/saved-price-alert')}
+              startIcon={<NotificationAddOutlined />}
+            >
+              Notifikasi Harga
+            </Button>
+          </Stack>
         </Grid>
       </Grid>
     </>
