0823786af0f5dddd839d79a0d5e55fd8db3f2e86
Riyan Ramadhan <ramadhanriyan3@gmail.com>
2024-02-14 15:52:03 +0700

    feat(kaboor):  integrate notifiction

--- src/components/features/Profil/Notification.tsx
+++ src/components/features/Profil/Notification.tsx
@@ -2,16 +2,24 @@ import { Dialog, Stack, Typography, Button } from '@mui/material';
 import PaymentNotif from './../../../assets/paymentNotif.svg';
 import { NotificationData } from '.';
 import CloseIcon from './../../../assets/close.svg';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { getDayMonth } from '.';
 import PaymentDetail from '../../shared/Profil/PaymentDetail';
-import { PaymentInfo, emptyPayment } from '../../shared/Profil';
+import { PaymentInfo } from '../../shared/Profil';
+import { useReactToPrint } from 'react-to-print';
 
 const Notification = () => {
   const jwtToken = localStorage.getItem('token');
   const [data, setData] = useState<NotificationData[]>([]);
-  const [paymentInfo, setPaymentInfo] = useState<PaymentInfo>(emptyPayment);
+  const [paymentInfo, setPaymentInfo] = useState<PaymentInfo>();
   const [isOpen, setIsOpen] = useState(false);
+
+  const printRef = useRef<HTMLDivElement>(null);
+  const handlePrint = useReactToPrint({
+    content: () => printRef.current,
+    onAfterPrint: () => setIsOpen(false),
+  });
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -32,28 +40,34 @@ const Notification = () => {
     fetchData();
   }, [jwtToken]);
 
-  const handlePopUp = (id: number) => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `https://fsw-backend.fly.dev/api/v1/booking/${id}/status`,
-          {
-            headers: {
-              Authorization: `Bearer ${jwtToken}`,
-            },
-          }
-        );
-        const notifData = await response.json();
-        setPaymentInfo(notifData.data);
-        console.log(notifData.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
-    setIsOpen(true);
+  const fetchingData = async (id: number) => {
+    try {
+      const response = await fetch(
+        `https://fsw-backend.fly.dev/api/v1/booking/${id}/status`,
+        {
+          headers: {
+            Authorization: `Bearer ${jwtToken}`,
+          },
+        }
+      );
+      const notifData = await response.json();
+      console.log('ini yang mau gua liat', notifData);
+      return notifData.data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-    console.log(paymentInfo);
+  const handlePopUp = async (id: number) => {
+    setPaymentInfo(await fetchingData(id));
+    console.log('1', await fetchingData(id));
+    console.log('2', paymentInfo);
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+    console.log('ini isOpen', isOpen);
   };
   return (
     <>
@@ -94,79 +108,93 @@ const Notification = () => {
           ></Typography>
         ) : (
           data.map((item, index) => (
-            <Stack
-              key={index}
-              direction={'row'}
-              justifyContent={'center'}
-              alignItems={'start'}
-              sx={{
-                width: '100%',
-                height: '100%',
-                padding: '8px 16px',
-                borderRadius: '8px',
-                border: '1px solid #C2C2C2',
-                background: 'white',
-                cursor: 'pointer',
-              }}
-              gap={2}
-              onClick={() => handlePopUp(Number(item.id))}
-            >
-              <img src={PaymentNotif} alt='icon' />
-              <Stack width={'100%'}>
-                <Stack
-                  direction={'row'}
-                  justifyContent={'space-between'}
-                  width={'100%'}
-                >
-                  <Typography
-                    sx={{
-                      color: '#1C1C1E',
-                      fontFamily: 'Open Sans',
-                      fontSize: '20px',
-                      fontWeight: 600,
-                      letterSpacing: '-0.75px',
-                    }}
-                  >{`${item.title}`}</Typography>
+            <>
+              <Stack
+                key={index}
+                direction={'row'}
+                justifyContent={'center'}
+                alignItems={'start'}
+                sx={{
+                  width: '100%',
+                  height: '100%',
+                  padding: '8px 16px',
+                  borderRadius: '8px',
+                  border: '1px solid #C2C2C2',
+                  background: 'white',
+                  cursor: 'pointer',
+                }}
+                gap={2}
+                onClick={() => handlePopUp(Number(item.id))}
+              >
+                <img src={PaymentNotif} alt='icon' />
+                <Stack width={'100%'}>
+                  <Stack
+                    direction={'row'}
+                    justifyContent={'space-between'}
+                    width={'100%'}
+                  >
+                    <Typography
+                      sx={{
+                        color: '#1C1C1E',
+                        fontFamily: 'Open Sans',
+                        fontSize: '20px',
+                        fontWeight: 600,
+                        letterSpacing: '-0.75px',
+                      }}
+                    >{`${item.title}`}</Typography>
+                    <Typography
+                      sx={{
+                        color: '#9E9E9E',
+                        fontFamily: 'Open Sans',
+                        fontSize: '16px',
+                        fontWeight: 600,
+                        letterSpacing: '-0.75px',
+                      }}
+                    >{`${getDayMonth(item.created_at)}`}</Typography>
+                  </Stack>
                   <Typography
                     sx={{
                       color: '#9E9E9E',
                       fontFamily: 'Open Sans',
-                      fontSize: '16px',
+                      fontSize: '18px',
                       fontWeight: 600,
                       letterSpacing: '-0.75px',
                     }}
-                  >{`${getDayMonth(item.created_at)}`}</Typography>
+                  >{`${item.detail}`}</Typography>
                 </Stack>
-                <Typography
-                  sx={{
-                    color: '#9E9E9E',
-                    fontFamily: 'Open Sans',
-                    fontSize: '18px',
-                    fontWeight: 600,
-                    letterSpacing: '-0.75px',
-                  }}
-                >{`${item.detail}`}</Typography>
               </Stack>
-              <Dialog open={isOpen}>
-                <Stack padding={2} gap={2}>
-                  <img
-                    src={CloseIcon}
-                    alt='icon'
-                    style={{ alignSelf: 'flex-end' }}
-                    onClick={() => setIsOpen(false)}
-                  />
-                  <PaymentDetail
-                    methodName={paymentInfo!.methodName}
-                    paymentDateTime={paymentInfo!.paymentDateTime}
-                    totalPrice={paymentInfo!.totalPrice}
-                    invoiceNumber={paymentInfo!.invoiceNumber}
-                  />
-                  <Button variant='outlined' color='secondary'>
-                    Download
-                  </Button>
-                </Stack>
-              </Dialog>
-            </Stack>
+              {paymentInfo ? (
+                <Dialog open={isOpen} hideBackdrop>
+                  <Stack padding={2} gap={2}>
+                    <img
+                      src={CloseIcon}
+                      alt='icon'
+                      style={{ alignSelf: 'flex-end', cursor: 'pointer' }}
+                      onClick={handleClose}
+                    />
+                    <div ref={printRef}>
+                      <PaymentDetail
+                        methodName={paymentInfo!.methodName}
+                        paymentDateTime={
+                          paymentInfo!.paymentDateTime
+                            ? paymentInfo!.paymentDateTime
+                            : paymentInfo.expiredTime
+                        }
+                        totalPrice={paymentInfo!.totalPrice}
+                        invoiceNumber={paymentInfo!.invoiceNumber}
+                      />
+                    </div>
+                    <Button
+                      variant='outlined'
+                      color='secondary'
+                      onClick={handlePrint}
+                    >
+                      Download
+                    </Button>
+                  </Stack>
+                </Dialog>
+              ) : null}
+            </>
           ))
         )}
       </Stack>
--- src/components/shared/Profil/PaymentDetail.tsx
+++ src/components/shared/Profil/PaymentDetail.tsx
@@ -16,171 +16,174 @@ interface propsType {
 const PaymentDetail = (props: propsType) => {
   return (
     <>
-      <Stack flexDirection={'column'} gap={'30px'} flexWrap={'wrap'}>
-        <Stack alignItems={'center'}>
-          <img src={SuccesIcon} alt='icon' width={'127px'} height={'127px'} />
-          <Typography
-            sx={{
-              color: '#1C1C1E',
-              textAlign: 'center',
-              fontSize: '20px',
-              fontWeight: 700,
-              lineHeight: '30px',
-              letterSpacing: '-0.75px',
-            }}
-          >
-            Pembayaran Berhasil
-          </Typography>
-        </Stack>
-        <Stack flexDirection={'row'} justifyContent={'space-between'}>
-          <Stack gap={1}>
-            <Typography
-              sx={{
-                color: '#9E9E9E',
-                fontSize: '18px',
-                fontWeight: 400,
-                lineHeight: '16px',
-                letterSpacing: '-0.15px',
-              }}
-            >
-              Nomor Faktur
-            </Typography>
+      <Stack justifyContent={'center'} alignItems={'center'} height={'100%'}>
+        <Stack flexDirection={'column'} gap={'30px'} flexWrap={'wrap'}>
+          <Stack alignItems={'center'}>
+            <img src={SuccesIcon} alt='icon' width={'127px'} height={'127px'} />
             <Typography
               sx={{
                 color: '#1C1C1E',
+                textAlign: 'center',
                 fontSize: '20px',
-                fontWeight: 600,
-                lineHeight: '28px',
+                fontWeight: 700,
+                lineHeight: '30px',
                 letterSpacing: '-0.75px',
               }}
             >
-              {props.invoiceNumber}
+              Pembayaran Berhasil
             </Typography>
           </Stack>
-          <Stack gap={1} alignItems={'flex-end'}>
-            <Typography
-              sx={{
-                color: '#9E9E9E',
-                fontSize: '18px',
-                fontWeight: 400,
-                textAlign: 'right',
-                lineHeight: '16px',
-                letterSpacing: '-0.15px',
-              }}
-            >
-              Metode Pembayaran
-            </Typography>
-            <Typography
-              sx={{
-                color: '#1C1C1E',
-                fontSize: '20px',
-                texAlign: 'right',
-                lineHeight: '28px',
-                letterSpacing: '-0.75px',
-              }}
-            >
-              Bank {props.methodName}
-            </Typography>
+          <Stack flexDirection={'row'} justifyContent={'space-between'}>
+            <Stack gap={1}>
+              <Typography
+                sx={{
+                  color: '#9E9E9E',
+                  fontSize: '18px',
+                  fontWeight: 400,
+                  lineHeight: '16px',
+                  letterSpacing: '-0.15px',
+                }}
+              >
+                Nomor Faktur
+              </Typography>
+              <Typography
+                sx={{
+                  color: '#1C1C1E',
+                  fontSize: '20px',
+                  fontWeight: 600,
+                  lineHeight: '28px',
+                  letterSpacing: '-0.75px',
+                }}
+              >
+                {props.invoiceNumber}
+              </Typography>
+            </Stack>
+            <Stack gap={1} alignItems={'flex-end'}>
+              <Typography
+                sx={{
+                  color: '#9E9E9E',
+                  fontSize: '18px',
+                  fontWeight: 400,
+                  textAlign: 'right',
+                  lineHeight: '16px',
+                  letterSpacing: '-0.15px',
+                }}
+              >
+                Metode Pembayaran
+              </Typography>
+              <Typography
+                sx={{
+                  color: '#1C1C1E',
+                  fontSize: '20px',
+                  texAlign: 'right',
+                  lineHeight: '28px',
+                  letterSpacing: '-0.75px',
+                }}
+              >
+                Bank {props.methodName}
+              </Typography>
+            </Stack>
           </Stack>
-        </Stack>
-        <Stack flexDirection={'row'} justifyContent={'space-between'}>
-          <Stack gap={1}>
-            <Typography
-              sx={{
-                color: '#9E9E9E',
-                fontSize: '18px',
-                fontWeight: 400,
-                lineHeight: '16px',
-                letterSpacing: '-0.15px',
-              }}
-            >
-              Tanggal
-            </Typography>
-            <Typography
-              sx={{
-                color: '#1C1C1E',
-                fontSize: '20px',
-                fontWeight: 600,
-                lineHeight: '28px',
-                letterSpacing: '-0.75px',
-              }}
-            >
-              {getFullDate(props.paymentDateTime)}
-            </Typography>
+          <Stack flexDirection={'row'} justifyContent={'space-between'}>
+            <Stack gap={1}>
+              <Typography
+                sx={{
+                  color: '#9E9E9E',
+                  fontSize: '18px',
+                  fontWeight: 400,
+                  lineHeight: '16px',
+                  letterSpacing: '-0.15px',
+                }}
+              >
+                Tanggal
+              </Typography>
+              <Typography
+                sx={{
+                  color: '#1C1C1E',
+                  fontSize: '20px',
+                  fontWeight: 600,
+                  lineHeight: '28px',
+                  letterSpacing: '-0.75px',
+                }}
+              >
+                {getFullDate(props.paymentDateTime)}
+              </Typography>
+            </Stack>
+            <Stack gap={1}>
+              <Typography
+                sx={{
+                  color: '#9E9E9E',
+                  fontSize: '18px',
+                  fontWeight: 400,
+                  lineHeight: '16px',
+                  letterSpacing: '-0.15px',
+                  textAlign: 'right',
+                }}
+              >
+                Pukul
+              </Typography>
+              <Typography
+                sx={{
+                  color: '#1C1C1E',
+                  fontSize: '20px',
+                  fontWeight: 600,
+                  lineHeight: '28px',
+                  letterSpacing: '-0.75px',
+                }}
+              >
+                {timeWithTimezone(new Date(props.paymentDateTime), 7)}
+              </Typography>
+            </Stack>
           </Stack>
-          <Stack gap={1}>
-            <Typography
-              sx={{
-                color: '#9E9E9E',
-                fontSize: '18px',
-                fontWeight: 400,
-                lineHeight: '16px',
-                letterSpacing: '-0.15px',
-              }}
-            >
-              Pukul
-            </Typography>
-            <Typography
-              sx={{
-                color: '#1C1C1E',
-                fontSize: '20px',
-                fontWeight: 600,
-                lineHeight: '28px',
-                letterSpacing: '-0.75px',
-              }}
-            >
-              {timeWithTimezone(new Date(props.paymentDateTime), 7)}
-            </Typography>
-          </Stack>
-        </Stack>
-        <Stack flexDirection={'row'} justifyContent={'space-between'}>
-          <Stack gap={1}>
-            <Typography
-              sx={{
-                color: '#9E9E9E',
-                fontSize: '18px',
-                fontWeight: 400,
-                lineHeight: '16px',
-                letterSpacing: '-0.15px',
-              }}
-            >
-              Jumlah Pembayaran
-            </Typography>
-            <Typography
-              sx={{
-                color: '#1C1C1E',
-                fontSize: '20px',
-                fontWeight: 600,
-                lineHeight: '28px',
-                letterSpacing: '-0.75px',
-              }}
-            >
-              {numToRp(props.totalPrice)}
-            </Typography>
-          </Stack>
-          <Stack gap={1}>
-            <Typography
-              sx={{
-                color: '#9E9E9E',
-                fontSize: '18px',
-                fontWeight: 400,
-                lineHeight: '16px',
-                letterSpacing: '-0.15px',
-              }}
-            >
-              Status
-            </Typography>
-            <Typography
-              sx={{
-                color: '#1C1C1E',
-                fontSize: '20px',
-                fontWeight: 600,
-                lineHeight: '28px',
-                letterSpacing: '-0.75px',
-              }}
-            >
-              Sukses
-            </Typography>
+          <Stack flexDirection={'row'} justifyContent={'space-between'}>
+            <Stack gap={1}>
+              <Typography
+                sx={{
+                  color: '#9E9E9E',
+                  fontSize: '18px',
+                  fontWeight: 400,
+                  lineHeight: '16px',
+                  letterSpacing: '-0.15px',
+                }}
+              >
+                Jumlah Pembayaran
+              </Typography>
+              <Typography
+                sx={{
+                  color: '#1C1C1E',
+                  fontSize: '20px',
+                  fontWeight: 600,
+                  lineHeight: '28px',
+                  letterSpacing: '-0.75px',
+                }}
+              >
+                {numToRp(props.totalPrice)}
+              </Typography>
+            </Stack>
+            <Stack gap={1}>
+              <Typography
+                sx={{
+                  color: '#9E9E9E',
+                  fontSize: '18px',
+                  fontWeight: 400,
+                  lineHeight: '16px',
+                  letterSpacing: '-0.15px',
+                }}
+              >
+                Status
+              </Typography>
+              <Typography
+                sx={{
+                  color: '#1C1C1E',
+                  fontSize: '20px',
+                  fontWeight: 600,
+                  lineHeight: '28px',
+                  letterSpacing: '-0.75px',
+                }}
+              >
+                Sukses
+              </Typography>
+            </Stack>
           </Stack>
         </Stack>
       </Stack>
